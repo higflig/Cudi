9739d4d18601375d70161596adc66e073bde6162
Thomas <tomwmth@users.noreply.github.com>
2019-12-26 22:29:14 +0800

    Factions top command

--- Modules/Factions.cs
+++ Modules/Factions.cs
@@ -1,5 +1,8 @@
 using System;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
@@ -8,10 +11,21 @@ namespace Cudi.Modules
 {
     public class Factions : ModuleBase<ShardedCommandContext>
     {
-        [Command("ping")]
-        public async Task TestAsync()
+        private readonly string ftopPath = "Resources/ftop.txt";
+        
+        [Command("ftop")]
+        public async Task GetFtopAsync()
         {
-            await ReplyAsync("Pong! :ping_pong:");
+            IEnumerable<string> lines = File.ReadAllLines(ftopPath).TakeLast(10);
+            var lastModify = (DateTime.Now - File.GetLastWriteTime(ftopPath)).TotalMinutes;
+
+            var eb = new EmbedBuilder()
+                .WithTitle($"Faction Top")
+                .WithDescription(string.Join('\n', lines))
+                .WithCurrentTimestamp()
+                .WithColor(Color.Blue)
+                .WithFooter($"Last updated {String.Format("{0:n0}", lastModify)} minutes ago");
+            await ReplyAsync(embed:eb.Build());
         }
     }
 }
